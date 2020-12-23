6cd35965b3a0e6718ee6eee9e9c2a78aa8467e8a
Sorin Alexandru Cocorada <sorin.cocorada@siemens.com>
2020-12-23 07:15:40 +0000

    Upload New File

---
+++ SPA/ui.js
@@ -0,0 +1,89 @@
+// Select DOM elements to work with
+const welcomeDiv = document.getElementById("WelcomeMessage");
+const signInButton = document.getElementById("SignIn");
+const cardDiv = document.getElementById("card-div");
+const mailButton = document.getElementById("readMail");
+const profileButton = document.getElementById("seeProfile");
+const profileDiv = document.getElementById("profile-div");
+
+function showWelcomeMessage(account) {
+    // Reconfiguring DOM elements
+    cardDiv.style.display = 'initial';
+    welcomeDiv.innerHTML = `Welcome ${account.username}`;
+    signInButton.setAttribute("onclick", "signOut();");
+    signInButton.setAttribute('class', "btn btn-success")
+    signInButton.innerHTML = "Sign Out";
+}
+
+
+
+
+function updateUI2(data){
+        console.log('updateUI2: ' + new Date().toString() + " "+data);
+        const response_display = document.getElementById("response_display");
+        const access_token = document.getElementById("access_token");
+
+        response_display.innerHTML = ''; // clear
+        response_display.innerHTML = "<strong>Full response: </strong><pre>" + JSON.stringify(data, null, 4)+"</pre>";
+
+
+	access_token.innerHTML = "<strong>Access Token: </strong><pre>" + data.accessToken +"</pre>";
+	id_token.innerHTML = "<strong>Id Token: </strong><pre>" + data.idToken +"</pre>";
+	id_tokenClaims.innerHTML = "<strong>Id Token Claims: </strong><pre>" + JSON.stringify(data.idTokenClaims, null, 4) +"</pre>";
+
+
+}
+
+function updateUI(data, endpoint) {
+    console.log('Graph API responded at: ' + new Date().toString() + " "+data);
+
+    if (endpoint === "https://graph.microsoft.com/v1.0/me") {
+        const name = document.createElement('p');
+        name.innerHTML = "<strong>displayName: </strong>" + data.displayName;
+        const title = document.createElement('p');
+        title.innerHTML = "<strong>Title: </strong>" + data.jobTitle;
+        const email = document.createElement('p');
+        email.innerHTML = "<strong>Mail: </strong>" + data.mail;
+        const phone = document.createElement('p');
+        phone.innerHTML = "<strong>Phone: </strong>" + data.businessPhones[0];
+        const address = document.createElement('p');
+        address.innerHTML = "<strong>Location: </strong>" + data.officeLocation;
+        profileDiv.appendChild(name);
+        profileDiv.appendChild(title);
+        profileDiv.appendChild(email);
+        profileDiv.appendChild(phone);
+        profileDiv.appendChild(address);
+
+    } else if (endpoint === "https://graph.microsoft.com/v1.0/me/messages") {
+        if (data.value.length < 1) {
+            alert("Your mailbox is empty!")
+        } else {
+            const tabContent = document.getElementById("nav-tabContent");
+            const tabList = document.getElementById("list-tab");
+            tabList.innerHTML = ''; // clear tabList at each readMail call
+
+            data.value.map((d, i) => {
+                // Keeping it simple
+                if (i < 10) {
+                    const listItem = document.createElement("a");
+                    listItem.setAttribute("class", "list-group-item list-group-item-action")
+                    listItem.setAttribute("id", "list" + i + "list")
+                    listItem.setAttribute("data-toggle", "list")
+                    listItem.setAttribute("href", "#list" + i)
+                    listItem.setAttribute("role", "tab")
+                    listItem.setAttribute("aria-controls", i)
+                    listItem.innerHTML = d.subject;
+                    tabList.appendChild(listItem)
+
+                    const contentItem = document.createElement("div");
+                    contentItem.setAttribute("class", "tab-pane fade")
+                    contentItem.setAttribute("id", "list" + i)
+                    contentItem.setAttribute("role", "tabpanel")
+                    contentItem.setAttribute("aria-labelledby", "list" + i + "list")
+                    contentItem.innerHTML = "<strong> from: " + d.from.emailAddress.address + "</strong><br><br>" + d.bodyPreview + "...";
+                    tabContent.appendChild(contentItem);
+                }
+            });
+        }
+    }
+}
