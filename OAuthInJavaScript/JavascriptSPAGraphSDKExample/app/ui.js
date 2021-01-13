7fd00ee887cb2349d68f09e14d4f7726c49bbcd5
Dimitar Dimitrov <dimitar.dimitrov@siemens.com>
2021-01-13 11:00:28 +0000

    fixing copyright

--- OAuthInJavaScript/JavascriptSPAGraphSDKExample/app/ui.js
+++ OAuthInJavaScript/JavascriptSPAGraphSDKExample/app/ui.js
@@ -1,11 +1,10 @@
 /*
 * Copyright (c) Siemens AG, 2020-2022
-*
-* Authors:
-*  OneAD team in cooperation with Comma Soft AG 
-*
+
 * This work is licensed under the terms of MIT.  See
 * the LICENSE file in the top-level directory.
+* Authors:
+*  OneAD team in cooperation with Comma Soft AG 
 */
 
 // Select DOM elements to work with
