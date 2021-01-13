ac6aee5bf2e1c47acd5dbc56a6dacc4bf5edd8df
Dimitar Dimitrov <dimitar.dimitrov@siemens.com>
2021-01-13 13:13:46 +0000

    adding header

--- SPA/graphConfig.js
+++ SPA/graphConfig.js
@@ -1,3 +1,8 @@
+/*
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+*  See LICENSE in the source repository root for complete license information.
+*/
+
 // Add here the endpoints for MS Graph API services you would like to use.
 const graphConfig = {
     graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
