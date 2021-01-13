740204706027d9489e6c09c3c10593f1633f286c
Dimitar Dimitrov <dimitar.dimitrov@siemens.com>
2021-01-13 11:11:22 +0000

    adding copyright

--- OAuthInCSharp/MicrosoftGraphAspNetCoreConnectSample/wwwroot/js/site.js
+++ OAuthInCSharp/MicrosoftGraphAspNetCoreConnectSample/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿// Write your Javascript code.
+/*
+* Copyright (c) Siemens AG, 2020-2022
+* This work is licensed under the terms of MIT.  See
+* the LICENSE file in the top-level directory.*
+* Authors:
+*  OneAD team in cooperation with Comma Soft AG 
+*/
+
+// Write your Javascript code.
