86b64756b4ae9c317c0de64dfbcf02800f1532c2
Dimitar Dimitrov <dimitar.dimitrov@siemens.com>
2021-01-13 10:59:12 +0000

    fixing copyright

--- OAuthInJavaScript/JavascriptSPAGraphSDKExample/app/authConfig.js
+++ OAuthInJavaScript/JavascriptSPAGraphSDKExample/app/authConfig.js
@@ -1,12 +1,12 @@
 /*
 * Copyright (c) Siemens AG, 2020-2022
 *
-* Authors:
-*  OneAD team in cooperation with Comma Soft AG 
-*
 * This work is licensed under the terms of MIT.  See
 * the LICENSE file in the top-level directory.
+* Authors:
+*  OneAD team in cooperation with Comma Soft AG 
 */
+
 
 
 // Config object to be passed to Msal on creation
