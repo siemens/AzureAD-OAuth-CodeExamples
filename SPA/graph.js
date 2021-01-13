95defd53c779d00811a61bacd65d566eabd67e0d
Dimitar Dimitrov <dimitar.dimitrov@siemens.com>
2021-01-13 13:13:27 +0000

    adding header

--- SPA/graph.js
+++ SPA/graph.js
@@ -1,3 +1,8 @@
+/*
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+*  See LICENSE in the source repository root for complete license information.
+*/
+
 /** 
  * Helper function to call MS Graph API endpoint
  * using the authorization bearer token scheme
